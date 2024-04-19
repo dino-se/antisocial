9d11fbb183e50445a85cd32dd719fa8b9f281513
EdisonP <133362311+edions@users.noreply.github.com>
2024-04-19 08:21:31 +0800

    Filter homepage to display follow user post only

--- api/content/getpost.php
+++ api/content/getpost.php
@@ -1,14 +1,22 @@
 <?php
+header('Content-type: application/json');
+
 include("../dbconnect.php");
 
+$fuid = $_GET['fuid'];
+
 try {
-    $query = "SELECT post.*, users.fullname 
+    $query = "SELECT *
               FROM post 
               INNER JOIN users ON post.user_id = users.user_id 
+              LEFT JOIN followers ON post.user_id = followers.following_id
+              WHERE followers.follower_id = :fuid
+              GROUP BY post.post_id
               ORDER BY post.post_id DESC";
-    $statement = $connection->prepare($query);
-    $statement->execute();
-    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $connection->prepare($query);
+    $stmt->bindParam(':fuid', $fuid);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     echo json_encode($result);
 } catch (PDOException $th) {
--- src/lib/home.js
+++ src/lib/home.js
@@ -1,24 +1,25 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            items: []
-        }
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      const cuid = localStorage.getItem('user_id');
+      fetch(`../api/content/getpost.php?fuid=${cuid}`)
+        .then((response) => response.json())
+        .then((data) => {
+          this.items = data;
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
     },
-    mounted() {
-        this.fetchData();
-    },
-    methods: {
-        fetchData() {
-            fetch('../api/content/getpost.php')
-                .then(response => response.json())
-                .then(data => {
-                    this.items = data;
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        }
-    }
+  },
 });
 
-app.mount('#homepage');
+app.mount("#homepage");
