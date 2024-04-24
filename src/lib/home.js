659f0107c7dd10875ad25d73d9176afa2d548505
EdisonP <133362311+edions@users.noreply.github.com>
2024-04-24 20:31:11 +0800

    Edit post content

--- src/lib/home.js
+++ src/lib/home.js
@@ -1,31 +1,51 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      items: [],
-    };
-  },
-  mounted() {
-    this.fetchPost();
-  },
-  methods: {
-    fetchPost() {
-      const cuid = localStorage.getItem('user_id');
-      fetch(`../api/content/get_content.php?fuid=${cuid}&cuid=${cuid}`)
-        .then((response) => response.json())
-        .then((data) => {
-          this.items = data;
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
+    data() {
+        return {
+            contents: [],
+        };
     },
-    deletePost(id) {
-      fetch(`../api/content/delete_content.php?post_id=${id}`)
-          .then(() => {
-              location.reload();
-          });
-  }
+    mounted() {
+        this.fetchPost();
+    },
+     methods: {
+        fetchPost() {
+            const cuid = localStorage.getItem('user_id');
+            fetch(`../api/content/get_content.php?fuid=${cuid}&cuid=${cuid}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    this.contents = data.map(content => ({
+                        ...content,
+                        isEditing: false,
+                        editedContent: content.content
+                    }));
+                })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+        },
+        editContent(id){
+            this.contents = this.contents.map(content => ({
+                ...content,
+                isEditing: content.post_id === id
+            }));
+        },
+        cancelEditContent(item) {
+            item.isEditing = false;
+            item.editedContent = item.content;
+        },
 
+        saveEditedContent(item) {
+            fetch(`../api/content/edit_content.php?postid=${item.post_id}&content=${item.editedContent}`)
+                .then(() => {
+                    location.reload();
+                });
+        },
+        deletePost(id) {
+            fetch(`../api/content/delete_content.php?post_id=${id}`)
+            .then(() => {
+                location.reload();
+            });
+        }
   }
 });
 
