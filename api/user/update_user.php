b23733df09bf16fc63ca561f52894eed4d5d1afc
EdisonP <133362311+edions@users.noreply.github.com>
2024-05-04 03:37:23 +0800

    feat: Upload profile picture

--- api/user/update_user.php
+++ api/user/update_user.php
@@ -5,14 +5,27 @@ if(isset($_POST['userid']) && isset($_POST['username'])) {
     $uid = $_POST['userid'];
     $fname = $_POST['fullname'];
     $uname = strtolower($_POST['username']);
+    $profile = $_FILES['profile'];
 
     try {
-        $query = "UPDATE users SET fullname = :fname, username = :uname
+
+        $original_filename = $_FILES['profile']['name'];
+        $file_extension = pathinfo($original_filename, PATHINFO_EXTENSION);
+        $filename = $uid . '_' . time() . '.' . $file_extension;
+
+        $target_directory = '../public/img/profile/';
+        $target_file = $target_directory . $filename;
+
+        move_uploaded_file($_FILES['profile']['tmp_name'], $target_file);
+        $target_file = str_replace("../", "/api/", $target_file);
+
+        $query = "UPDATE users SET fullname = :fname, username = :uname, profile_pic = :pic
                   WHERE user_id = :uid";
         $stmt = $conn->prepare($query);
         $stmt->bindParam(':uid', $uid);
         $stmt->bindParam(':fname', $fname);
         $stmt->bindParam(':uname', $uname);
+        $stmt->bindParam(':pic', $target_file);
         $stmt->execute();
 
         if ($stmt->rowCount() > 0) {
