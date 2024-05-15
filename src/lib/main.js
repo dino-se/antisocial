7dc1685f0e760c19c208f85a3bc80876e2385e28
EdisonP <133362311+edions@users.noreply.github.com>
2024-05-15 20:31:11 +0800

    Notif improvements

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,22 +1,32 @@
+const cuid = localStorage.getItem('user_id');
+
 const main = Vue.createApp({
     data() {
         return {
             uid: "",
             sugg: [],
+            notiff: 0
         }
     },
     created() {
         this.uid = localStorage.getItem('user_id');
+        this. getAllNotif();
         this.suggestions();
     },
     methods: {
         suggestions() {
-            const cuid = localStorage.getItem('user_id');
             fetch(`../api/user/suggest_user.php?uid=${cuid}`)
             .then((data) => data.json())
             .then((result) => {
                 this.sugg = result;
             });
+        },
+        getAllNotif() {
+            fetch(`../api/user/notif_user.php?uid=${cuid}`)
+            .then((data) => data.json())
+            .then((res) => {
+                this.notiff = res[0].total_likes;
+            })
         }
     }
 });
